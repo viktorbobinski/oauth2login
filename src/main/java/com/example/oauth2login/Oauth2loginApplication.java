5a87c545f1da326ddd174316183a40e292f80e4d
vikbo <vikbobinski@gmail.com>
2020-05-06 11:56:30 +0200

    Initial commit

---
+++ src/main/java/com/example/oauth2login/Oauth2loginApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2loginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2loginApplication.class, args);
+	}
+
+}
