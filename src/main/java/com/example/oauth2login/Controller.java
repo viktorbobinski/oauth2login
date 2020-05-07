cdc17e03b540136fc39072a6c98d50d2499e98a9
vikbo <vikbobinski@gmail.com>
2020-05-07 12:30:57 +0200

    Added google authorization

---
+++ src/main/java/com/example/oauth2login/Controller.java
@@ -0,0 +1,18 @@
+package com.example.oauth2login;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/user")
+    public String restricted() {
+        return "Hello User!";
+    }
+}
---
+++ src/main/java/com/example/oauth2login/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.example.oauth2login;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests(r -> r
+                        .antMatchers("/").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login();
+    }
+}
