97fb3c903a8b7bdde22979e14421815271236a91
vikbo <vikbobinski@gmail.com>
2020-05-11 18:08:34 +0200

    Test for UserRepository

--- src/main/java/com/example/oauth2login/Controller.java
+++ src/main/java/com/example/oauth2login/Controller.java
@@ -4,10 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
-
 @RestController
 public class Controller {
 
